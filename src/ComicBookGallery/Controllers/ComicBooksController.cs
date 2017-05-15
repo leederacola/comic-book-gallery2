9e4f2a1df2e383dd46bc36bc52a5b7adb2bd4e6c
leederacola <rmgforce@gmail.com>
2017-05-15 14:05:43 -0400

    added comic book repo

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,21 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook() //object initializer syntax
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artist = new Artists[]
-                {
-                    new Models.Artists() {Name = "Dan Slott", Role = "Script" },
-                    new Models.Artists() {Name = "Humberto Ramos", Role = "Pencils" },
-                    new Models.Artists() {Name = "Victor Olazaba", Role = "Inks" },
-                    new Models.Artists() {Name = "Edgar Delgado", Role = "Colors" },               
-                    new Models.Artists() {Name = "Chris Eliopoulos", Role = "Letters" },
-                }
-
-        };          
+            
      
             return View(comicBook); 
         }
